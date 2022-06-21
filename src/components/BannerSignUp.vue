9f0b04b1b0ef9ae501cc7decfa5143959cb49572
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-06-21 18:06:45 +0200

    aggiunta ciclo socials

--- src/components/BannerSignUp.vue
+++ src/components/BannerSignUp.vue
@@ -4,11 +4,9 @@
             <a class="myButton text-uppercase">sign-up now</a>
             <div class="d-flex gap-3">
                 <div class="fs-5">follow us</div>
-                <img src="/img/footer-facebook.png" alt="">
-                <img src="/img/footer-twitter.png" alt="">
-                <img src="/img/footer-youtube.png" alt="">
-                <img src="/img/footer-pinterest.png" alt="">
-                <img src="/img/footer-periscope.png" alt="">
+                <a v-for="(social,id) in socialList" :key="id" :href="social.href">
+                    <img :src="social.img" :alt="social.alt">
+                </a>
             </div>
         </div> 
     </div>
@@ -17,6 +15,37 @@
 <script>
 export default {
 name: 'BannerSignUp',
+data() {
+    return {
+        socialList: [
+            {
+                img: '/img/footer-facebook.png',
+                href: '#',
+                alt: 'facebook'
+            },
+            {
+                img: '/img/footer-twitter.png',
+                href: '#',
+                alt: 'twitter'
+            },
+            {
+                img: '/img/footer-youtube.png',
+                href: '#',
+                alt: 'youtube'
+            },
+            {
+                img: '/img/footer-pinterest.png',
+                href: '#',
+                alt: 'pinterest'
+            },
+            {
+                img: '/img/footer-periscope.png',
+                href: '#',
+                alt: 'periscope'
+            },
+        ]
+    }
+}
 };
 </script>
 
